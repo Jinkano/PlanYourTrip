075ab14e04d21c000f8d62e85e487b4c2f5e1c69
jinkis <jwir79@gmail.com>
2025-11-24 13:25:46 +0100

    Creación del formulario para registrar un usuario

--- PlanYourTrip/ViewController.swift
+++ PlanYourTrip/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    /**/
+    
+    @IBOutlet weak var tfUsername: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    /**/
+    
+    @IBAction func singUp(_ sender: Any)
+    {
+        //
+        let email = tfUsername.text ?? ""
+        let password = tfPassword.text ?? ""
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+          //
+            if let error = error
+            {
+                print(error.localizedDescription)
+                return
+            }
+            
+            print("Usuario registrado")
+            
+        }
+    }
+    
+    @IBAction func signIn(_ sender: Any)
+    {
+        //
+        let email = tfUsername.text ?? ""
+        let password = tfPassword.text ?? ""
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+          //
+            if let error = error
+            {
+                print(error.localizedDescription)
+                return
+            }
+            
+            print("Ingreso autoriazado")
+        }
+    }
+    
+    
 }
 
